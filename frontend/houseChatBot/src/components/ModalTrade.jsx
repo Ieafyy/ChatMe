2659224490a5755b68497d1bd19c7ba6b212694c
Leafy <raphaelsilvaramalho.com@gmail.com>
2024-09-25 16:05:23 -0300

    FEAT: Troca de models

--- frontend/houseChatBot/src/components/ModalTrade.jsx
+++ frontend/houseChatBot/src/components/ModalTrade.jsx
@@ -4,8 +4,7 @@ function ModalTrade({ isOpen, onClose, setActualModel, actualModel }) {
 
     const [AllModels, setAllModels] = React.useState([])
 
-    React.useEffect(() => {
-        console.log('modal!')
+    const getModels = () => {
         fetch('http://192.168.0.14:5000/api/getModels').then(response => {
             if (!response.ok) throw new Error('erro!')
             return response.json()  
@@ -13,6 +12,11 @@ function ModalTrade({ isOpen, onClose, setActualModel, actualModel }) {
             console.log(data)
             setAllModels(data['models'])
         })
+    }
+
+    React.useEffect(() => {
+        console.log('modal!')
+        getModels()
     }, [])
 
     const handleChange = (event) => {
@@ -24,7 +28,7 @@ function ModalTrade({ isOpen, onClose, setActualModel, actualModel }) {
         <div className="fixed inset-0 z-10 flex items-center justify-center bg-gray-400 bg-opacity-50">
             <div className="bg-white text-black px-20 py-8 rounded-xl border-2 border-black text-center">
                 <h2 className="mb-2 font-bold text-3xl mb-5">Selecione o modelo</h2>
-                <select id="dropdown" value={actualModel} onChange={handleChange} className="border-2 border-black text-xl mx-5 rounded-xl py-2 px-2 bg-slate-200">
+                <select onMouseEnter={getModels} onClick={getModels} id="dropdown" value={actualModel} onChange={handleChange} className="border-2 border-black text-xl mx-5 rounded-xl py-2 px-2 bg-slate-200">
                     {AllModels.map((option) => (
                     <option key={option} value={option}>
                         {option}
